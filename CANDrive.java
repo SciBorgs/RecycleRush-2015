40f5ecef0a019cc1be0e31774c1680d128f8a777
Lucas Cecchi <marcz@bxscience.edu>
2015-01-08 17:11:42 -0500

    Update CANDrive.java

--- CANDrive.java
+++ CANDrive.java
@@ -5,14 +5,21 @@ public class CANDrive {
     CANTalon frontRightTalon = new CANTalon(2);
     CANTalon backLeftTalon = new CANTalon(3);
     CANTalon backRightTalon = new CANTalon(4);
-
+    
+    CANTalon testVertTalon = new CANTalon(5);
+    CANTalon testHorzTalon = new CANTalon(6);
+    
     Joystick leftJoy = new Joystick(1);
     Joystick rightJoy = new Joystick(2);
     
-    rightVal = Hardware.rightJoy.getY();
-    leftVal = Hardware.leftJoy.getY();
+    double rightVal = Hardware.rightJoy.getY();
+    double leftVal = Hardware.leftJoy.getY();
+    
+    public double horzSpeed = testHorzTalon.getSpeed();
+    public double vertSpeed = testVertTalon.getSpeed();
     
     public void function(){
+    
     
       if(leftVal > 0.1 || leftVal < -0.1){
             Hardware.frontRightTalon.set(leftVal);

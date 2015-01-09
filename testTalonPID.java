9544c08202ee899112fd860142aa2a4f085dfcdd
Lucas Cecchi <marcz@bxscience.edu>
2015-01-09 15:43:17 -0500

    Update testTalonPID.java

--- testTalonPID.java
+++ testTalonPID.java
@@ -2,38 +2,4 @@ package roboticstest;
 
 public class testTalonPID extends PIDController{
 
-    CANTalon liftClaw = new CANTalon(1);
-    
-    public liftClaw {
-      super("ClawLift", 2.0, 0.0, 0.0);
-      setAbsoluteTolerance(0.05);
-      getPIDController().setContinuous(false);
-    }
-    
-    
-   
-    //probably none of this actually works
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    //victor motor...lol
+}

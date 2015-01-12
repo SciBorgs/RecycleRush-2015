c55f8cc88e4103223695d94b4076697b2d424b98
yorhodes <rhodesy@bxscience.edu>
2015-01-12 17:53:22 -0500

    Update Dashboard.java

--- Dashboard.java
+++ Dashboard.java
@@ -1,9 +1,9 @@
 public class Dashboard {
   public void initialize() {
-    //declare software interfaces
+    //declare hardware components to interact with --> moved to Hardware class later
+    public BuiltInAccelerometer accelerometer = new BuiltInAccelerometer();
     
     //RoboRIO builtin accelerometer
-    private BuiltInAccelerometer accelerometer = new BuiltInAccelerometer();
     SmartDashboard.putNumber("X: ", accelerometer.getX());
     SmartDashboard.putNumber("Y: ", accelerometer.getY());
     SmartDashboard.putNumber("Z: ", accelerometer.getZ());

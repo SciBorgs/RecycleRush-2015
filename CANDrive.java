e3a9373af7e59fcccb4286c9d1179e11ff03df9f
Yoli Meydan <meydany@bxscience.edu>
2015-01-13 16:32:50 -0500

    Cleaned code up a bit

--- CANDrive.java
+++ CANDrive.java
@@ -1,6 +1,7 @@
 package roboticstest;
 
 public class CANDrive {
+    
     CANTalon frontLeftTalon = new CANTalon(1);
     CANTalon frontRightTalon = new CANTalon(2);
     CANTalon backLeftTalon = new CANTalon(3);
@@ -12,27 +13,33 @@ public class CANDrive {
     Joystick leftJoy = new Joystick(1);
     Joystick rightJoy = new Joystick(2);
     
-    double rightVal = rightJoy.getY();
-    double leftVal = leftJoy.getY();
+    double rightVal;
+    double leftVal; 
     
-    public double horzSpeed = testHorzTalon.getSpeed();
-    public double vertSpeed = testVertTalon.getSpeed();
+    //public double horzSpeed = testHorzTalon.getSpeed();
+    //public double vertSpeed = testVertTalon.getSpeed();
     
+    //Gets value from left/right joysticks and sets to talons
     public void function(){
-    
-    
-      if(leftVal > 0.1 || leftVal < -0.1){
+        
+        //Getting values from each joystick
+        rightVal = rightJoy.getY();
+        leftVal = leftJoy.getY();
+        
+        //Setting values for left and right talons
+        if(leftVal > 0.1 || leftVal < -0.1){
             frontRightTalon.set(leftVal);
             backRightTalon.set(leftVal);
-        }else{
+        }
+        else{
             frontRightTalon.set(0);
             backRightTalon.set(0);
         }
-        //same deal but for right joystick
         if(rightVal > 0.1 || rightVal < -0.1){
             frontLeftTalon.set(-rightVal);
             backLeftTalon.set(-rightVal);
-        }else{
+        }
+        else{
             frontLeftTalon.set(0);
             backLeftTalon.set(0);
         }

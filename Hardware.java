d56a4bc28a5e26d74974f590380c45b61c3fd9ae
yorhodes <rhodesy@bxscience.edu>
2015-01-13 23:13:36 -0500

    Update Hardware.java

--- Hardware.java
+++ Hardware.java
@@ -12,22 +12,29 @@ public class Hardware {
   public static CANTalon leftArmTalon;
   public static CANTalon rightArmTalon;
   public static Encoder armEncoder;
-  public static Solenoid clawSol;
+  public static DoubleSolenoid clawSol;
+  
+  //miscellaneous
+  public static Compressor compressor;
   
   //driver station
   public static DriverStationLCD dLCD;
 
   public Hardware(){
     //drive chassis hardware
-    frontLeftTalon = new CANTalon(1); //device ID numbers subject to change
-    frontRightTalon = new CANTalon(2); 
+    frontLeftTalon = new CANTalon(1); //device CAN pcm ID numbers subject to change
+    frontRightTalon = new CANTalon(2);
     backLeftTalon = new CANTalon(3); 
     backRightTalon = new CANTalon(4); 
     
     //claw hardware
     leftArmTalon = new Talon(5); 
     rightArmTalon = new Talon(6);
-  
+    leftArmTalon.setFeedbackDevice(armEncoder);
+    //rightArmTalon.setFeedbackDevice(armEncoder);
+    clawSol = new DoubleSolenoid(); //
     
+    //miscellaneous
+    compressor = new Compressor(7); //device CAN pcm ID numbers subject to change
   }
 }

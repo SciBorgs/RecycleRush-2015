181fe323143600e0f15fc916c82391558a779ddb
Kshitiz Sharma <sharmak@bxscience.edu>
2015-01-16 16:21:55 -0500

    Update OI.java

--- src/org/usfirst/frc/team1155/robot/OI.java
+++ src/org/usfirst/frc/team1155/robot/OI.java
@@ -17,8 +17,15 @@ public class OI {
     // One type of button is a joystick button which is any button on a joystick.
     // You create one by telling it which joystick it's on and which button
     // number it is.
-    // Joystick stick = new Joystick(port);
+    Joystick leftJoy = new Joystick(1);
+    Joystick rightJoy = new Joystick(2);
+    Joystick gamePad = new Joystick(3);
     // Button button = new JoystickButton(stick, buttonNumber);
+    Button leftButton1 = new JoystickButton(leftJoy, 1), leftButton2 = new JoystickButton(leftJoy, 2); //change button numbers
+    Button rightButton1 = new JoystickButton(rightJoy, 1), rightButton2 = new JoystickButton(rightJoy, 2); //change button numbers
+    
+    Trigger lTrigger = new JoystickButton(leftJoy, 1); //change button numbers
+    Trigger rTrigger = new JoystickButton(rightJoy, 1); //change button numbers
     
     // There are a few additional built in buttons you can use. Additionally,
     // by subclassing Button you can create custom triggers and bind those to

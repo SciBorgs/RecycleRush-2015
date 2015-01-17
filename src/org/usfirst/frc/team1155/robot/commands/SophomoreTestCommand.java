ad14a2d079abe7a6df55480b0a81c7cf3d885b50
meydany <meydany@011-30.bxsci.78x445.nycboe.org>
2015-01-17 14:48:16 -0500

    Sophomores working on an accelerometer movement command

---
+++ src/org/usfirst/frc/team1155/robot/commands/SophomoreTestCommand.java
@@ -0,0 +1,81 @@
+package org.usfirst.frc.team1155.robot.commands;
+
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+
+//This is a command that can be used to move forward slightly
+public class SophomoreTestCommand extends Command{
+	
+    private double yVal;
+    private double xVal;
+    
+    private double oldAccel = 0;
+    private double currentAccel = 0;
+    private double changeInXAccel = 0;
+    private double changeInYAccel = 0;
+    
+    private double oldTime = 0;
+    private double currentTime = 0;
+    private double changeInTime = 0;
+    
+    private double xDisplacement;
+    private double yDisplacement;
+    
+    Timer timer;
+	BuiltInAccelerometer accel;
+
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+		accel = new BuiltInAccelerometer();
+		timer = new Timer();
+	}
+
+	@Override
+	protected void execute() {
+		oldTime = currentTime;
+		timer.start();
+		
+		//gets the change in acceleration over the x axis after one tick
+		oldAccel = currentAccel;
+		currentAccel = accel.getX();
+		changeInXAccel = currentAccel - oldAccel;
+		
+		xDisplacement = getDisplacement(changeInXAccel, changeInTime);
+		
+		//gets the change in acceleration over the y axis after one tick
+		oldAccel = currentAccel;
+		currentAccel = accel.getY();
+		changeInYAccel = currentAccel - oldAccel;
+		
+		yDisplacement = getDisplacement(changeInYAccel, changeInTime);
+
+	    currentTime = timer.get();
+		changeInTime = currentTime - oldTime;
+	}
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	private double getDisplacement (double changeInAccel, double changeInTime){
+		changeInAccel *= 9.81;
+		return 0.25 * (changeInAccel*changeInTime*changeInTime);
+	}
+}
+//dereggs//

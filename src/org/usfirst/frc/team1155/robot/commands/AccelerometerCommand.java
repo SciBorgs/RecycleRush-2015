49f97f4362f141555f6530e59f382f940e705efd
Yoli Meydan <meydany@bxscience.edu>
2015-01-20 16:50:55 -0500

    Finished accelerometer command

--- src/org/usfirst/frc/team1155/robot/commands/AccelerometerCommand.java
+++ src/org/usfirst/frc/team1155/robot/commands/AccelerometerCommand.java
@@ -5,26 +5,21 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 //This is a command that can be used to move forward slightly
-public class SophomoreTestCommand extends Command{
+public class AccelerometerCommand extends Command{
 	
-    private double yVal;
-    private double xVal;
-    
-    private double oldXAccel = 0;
     private double oldYAccel = 0;
     
-    private double currentXAccel = 0;
     private double currentYAccel = 0;
     
-    private double changeInXAccel = 0;
     private double changeInYAccel = 0;
     
     private double oldTime = 0;
     private double currentTime = 0;
     private double changeInTime = 0;
     
-    private double xDisplacement;
-    private double yDisplacement;
+    private double distance;
+    
+    private double speed;
     
     Timer timer;
 	BuiltInAccelerometer accel;
@@ -33,35 +28,46 @@ public class SophomoreTestCommand extends Command{
 	protected void initialize() {
 		accel = new BuiltInAccelerometer();
 		timer = new Timer();
+		timer.start();
 	}
 
 	@Override
 	protected void execute() {
 		oldTime = currentTime;
-		timer.start();
 		
-		//gets the change in acceleration over the x axis after one tick
-		oldXAccel = currentXAccel;
 		oldYAccel = currentYAccel;
 		
-		currentXAccel = accel.getX();
 		currentYAccel = accel.getY();
-		
-		changeInXAccel = currentXAccel - oldXAccel;
+
 		changeInYAccel = currentYAccel - oldYAccel;
 		
-		xDisplacement = getDisplacement(changeInXAccel, changeInTime);
-		yDisplacement = getDisplacement(changeInYAccel, changeInTime);
-
-	    	currentTime = timer.get();
+		currentTime = timer.get();
 		changeInTime = currentTime - oldTime;
+		
+		distance = changeInYAccel * changeInTime * changeInTime;
+		speed = distance/changeInTime;
 	}
-
+	
+	public double returnDistance() {
+		return distance;
+	}
+	
+	public double getX() {
+		return accel.getX();
+	}
+	
+	public double getY() {
+		return accel.getY();
+	}
+	
+	public double getZ() {
+		return accel.getZ();
+	}
+	
 	@Override
 	protected void end() {
 		
 	}
-
 	@Override
 	protected void interrupted() {
 		
@@ -71,10 +77,4 @@ public class SophomoreTestCommand extends Command{
 	protected boolean isFinished() {
 		return false;
 	}
-	
-	private double getDisplacement (double changeInAccel, double changeInTime){
-		changeInAccel *= 9.81;
-		return 0.25 * (changeInAccel*changeInTime*changeInTime);
-	}
 }
-//dereggs//

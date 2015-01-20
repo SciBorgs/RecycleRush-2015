0eb5f79dc7f32494dc0e244ece55ccc73bbcc9b8
kothekaa <kothekaa@bxscience.edu>
2015-01-20 16:20:15 -0500

    Added Gyro and Ultrasonic data display

--- src/org/usfirst/frc/team1155/robot/Dashboard.java
+++ src/org/usfirst/frc/team1155/robot/Dashboard.java
@@ -7,10 +7,24 @@ public class Dashboard {
   public void initialize() {
     //declare hardware components to interact with --> moved to Hardware class later
     BuiltInAccelerometer accelerometer = new BuiltInAccelerometer();
+    Gyro gyro = new Gyro();
+    Ultrasonic sonic = new UltraSonic()
+    
+    //Hardware hardware = new Hardware();
+    //getGyro, getUltrasonic --> return objects
     
     //RoboRIO builtin accelerometer
+    SmartDashboard.putString("Accelerometer Data", new String(""));
     SmartDashboard.putNumber("X: ", accelerometer.getX());
     SmartDashboard.putNumber("Y: ", accelerometer.getY());
     SmartDashboard.putNumber("Z: ", accelerometer.getZ());
+    
+    //gyro angle
+    SmartDashboard.putString("Gyro Data", new String(""));
+    SmartDashboard.putNumber("Angle: ",gyro.getAngle());
+    
+    //ultrasonic
+    SmartDashboard.putString("Ultrasonic Data", new String(""));
+    SmartDashboard.putNumber("Distance",sonic.getDistanceUnits());
   }
 }

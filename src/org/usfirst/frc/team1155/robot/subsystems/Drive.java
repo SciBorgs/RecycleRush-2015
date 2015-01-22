b9f099d69a8d3ce72a466051bbe02533c0f6134e
abot98 <abot98 arunleob@gmail.com>
2015-01-22 16:52:06 -0500

    Configuation Test with new build paths

--- src/org/usfirst/frc/team1155/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team1155/robot/subsystems/Drive.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team1155.robot.subsystems;
 
+import org.usfirst.frc.team1155.robot.Hardware;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Drive extends Subsystem {
@@ -13,5 +15,9 @@ public class Drive extends Subsystem {
 		
 	}
 	
+	public void set(double leftSpeed, double rightSpeed) {
+		//Drive curve code goes here
+
+	}
 	
 }

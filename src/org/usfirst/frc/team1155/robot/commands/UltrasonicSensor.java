32be28a859a2c784d2a57c97adf42cdb3eabf5c2
sternm <sternm@011-11.bxsci.78x445.nycboe.org>
2015-01-24 11:01:47 -0500

    Added UltrasonicSensor command

---
+++ src/org/usfirst/frc/team1155/robot/commands/UltrasonicSensor.java
@@ -0,0 +1,60 @@
+package org.usfirst.frc.team1155.robot.commands;
+
+import org.usfirst.frc.team1155.robot.Hardware;
+
+import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class UltrasonicSensor extends Command{
+	private Ultrasonic leftUltrasonic, rightUltrasonic, frontUltrasonic;
+	
+	public double leftDist = 0, rightDist = 0, frontDist = 0;
+	
+	public UltrasonicSensor() {
+		leftUltrasonic = Hardware.INSTANCE.leftUltrasonic;
+		rightUltrasonic = Hardware.INSTANCE.rightUltrasonic;
+		frontUltrasonic = Hardware.INSTANCE.frontUltrasonic;
+	}
+	
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+		leftUltrasonic.setAutomaticMode(true);
+		rightUltrasonic.setAutomaticMode(true);
+		frontUltrasonic.setAutomaticMode(true);
+		
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+		if (leftUltrasonic.isRangeValid())
+			leftDist = leftUltrasonic.getRangeInches();
+		if (rightUltrasonic.isRangeValid())
+			rightDist = rightUltrasonic.getRangeInches();
+		if (frontUltrasonic.isRangeValid())
+			frontDist = frontUltrasonic.getRangeInches();
+		
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return (!leftUltrasonic.isEnabled() && !rightUltrasonic.isEnabled() && !frontUltrasonic.isEnabled());
+	}
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+		leftUltrasonic.free();
+		rightUltrasonic.free();
+		frontUltrasonic.free();
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/org/usfirst/frc/team1155/robot/subsystems/Sensors.java
+++ src/org/usfirst/frc/team1155/robot/subsystems/Sensors.java
@@ -1,5 +1,26 @@
 package org.usfirst.frc.team1155.robot.subsystems;
 
-public class Sensors {
+import org.usfirst.frc.team1155.robot.Hardware;
+
+import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Sensors extends Subsystem{
+	public Ultrasonic leftUltrasonic, rightUltrasonic, frontUltrasonic;
+	
+	public Sensors() {
+		leftUltrasonic = Hardware.INSTANCE.leftUltrasonic;
+		rightUltrasonic = Hardware.INSTANCE.rightUltrasonic;
+		frontUltrasonic = Hardware.INSTANCE.frontUltrasonic;
+	}
+	
+	
+	
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }

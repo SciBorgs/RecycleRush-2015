ce783b244903e1a15bcd84b61e41e6448b5cb026
abot98 <arunleob@gmail.com>
2015-03-25 23:44:07 -0400

    Ultrasonic Update

---
+++ src/org/usfirst/frc/team1155/robot/commands/Delay.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team1155.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class Delay extends Command {
+
+	long delay;
+	long startTime;
+	public Delay(int delay) {
+		this.delay = delay * (int) Math.pow(10, 9);
+	}
+	
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+		startTime = System.nanoTime();
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return startTime + delay < System.nanoTime();
+	}
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
